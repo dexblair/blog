fe62438fe3fa09a0e201f0a87afceaf5079dd63c
Dex <dex@dexs-macbook.local>
2012-11-20 21:05:02 -0500

    better blogengine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '64203ef0037d39cc953fd465972e6d7526bec8df7ae5c1d1d679f96b7cdb5976009b9ad4b12fc788139851c5e37e29cb7a34d7ce04af1bad4f8258fd3d4d4018'
